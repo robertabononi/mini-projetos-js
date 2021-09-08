e81ec64946c5a55a27b46f4a09c54aaf7857ee23
Roberta Bononi da Silva <roberta.bononi16@gmail.com>
2021-09-08 14:20:04 -0300

    feat: calculate imc and show result

--- imc/imc.js
+++ imc/imc.js
@@ -1,5 +1,34 @@
-const nome = document.getElementById('nome');
-const altura = document.getElementById('altura');
-const peso = document.getElementById('peso');
 const calcular = document.getElementById('calcular');
-const resultado = document.getElementById('resultado');
+
+function imc() {
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome.value !== '' && altura !== '' && peso !== '') {
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let classificacao = "";
+
+        if (valorIMC < 18.5) {
+            classificacao = "abaixo do peso.";
+        } else if (valorIMC <= 24.9) {
+            classificacao = "com peso ideal. Parabéns!";
+        } else if (valorIMC <= 29.9) {
+            classificacao = "com pré-obesidade. Procure um médico.";
+        } else if (valorIMC <= 34.9) {
+            classificacao = "com obesidade grau um. Procure um médico.";
+        } else if (valorIMC <= 39.9) {
+            classificacao = "com obesidade grau dois. Procure um médico.";
+        } else {
+            classificacao = "com obesidade grau três/mórbida. Procure um médico.";
+        }
+
+        resultado.textContent = `${nome}, seu IMC é ${valorIMC}. Você está ${classificacao}`;
+    } else {
+        resultado.textContent = 'Preencha todos os campos.';
+    }
+}
+
+calcular.addEventListener('click', imc);
